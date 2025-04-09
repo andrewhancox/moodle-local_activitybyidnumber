169829768e7fa534554ce28561fdcde6847e28fb
Andrew Hancox <andrewdchancox@googlemail.com>
2025-04-09 11:47:26 +0100

    Bump versions

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025040100;
+$plugin->version   = 2025040900;
 $plugin->requires  = 2022112809;
 $plugin->component = 'local_activitybyidnumber';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "1.0.0";
+$plugin->release   = "1.0.1";
